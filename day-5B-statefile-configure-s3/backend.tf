5b3b6fdfb91f81ea74fbee2fcd54f48c80bf46c8
shubham <shubhamshetkar46@gmail.com>
2024-08-28 21:45:25 +0530

    day5 changes

---
+++ day-5B-statefile-configure-s3/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "nareshawsdevv"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, 
+    # note: first run "day-5A-resources-s3-dynamodb-for-statefile" to create s3 and dynamodb resources
+    encrypt = true  # Ensures the state is encrypted at rest in S3.
+  }
+}
