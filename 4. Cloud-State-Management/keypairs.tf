d490ffd0151024479436ebc72f94f735f6fed555
Shubham shetkar <167677582+shub0618@users.noreply.github.com>
2025-01-06 10:49:23 +0530

    update

--- 4. Cloud-State-Management/keypairs.tf
+++ 4. Cloud-State-Management/keypairs.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "vprofilekey" {
   key_name   = "vprofilekey"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHvnHVG7Ob3D1f4hgX0FEl/kdHvyDcmuYphGbYnZ7j2 shubh@Shubham"
+  public_key = "paste the public key"
 }
